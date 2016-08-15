47597a1ff8c3d4fa8e221702182b232e5c234424
Matheus Victor <hffmatheus@gmail.com>
2016-08-14 22:20:39 -0300

    no message

--- ember2-blog/app/components/create-new-post/component.js
+++ ember2-blog/app/components/create-new-post/component.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    createPost: function () {
-      console.log("Hello!");
+    createPost: function (newPost) {
+      console.log(newPost);
+      console.log(newPost.title, newPost.author);
     }
   }
 });
