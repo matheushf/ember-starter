cde8d8b2c3423c50034e392c0760f90bbc0feeac
Matheus Victor <hffmatheus@gmail.com>
2016-08-30 21:42:21 -0300

    adjustments

---
+++ library-app/app/controllers/index.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  emailAddress: '',
+
+  isDisabled: Ember.computed.empty('emailAddress'),
+
+  actualEmailAddress: Ember.computed('emailAddress', function () {
+    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+  }),
+
+  emailAddressChanged: Ember.observer('emailAddress', function() {
+    console.log('observer is called', this.get('emailAddress'));
+  })
+
+});
